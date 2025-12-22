cee4cd4c77c4f1d4b916dac5d33524d08156f195
Alexander Morugin <nobilis@bk.ru>
2025-12-22 20:18:02 +0300

    animate header

--- components/header/index.vue
+++ components/header/index.vue
@@ -3,21 +3,21 @@
     <WrapperPadding>
       <div class="header__container">
         <div class="header__left">
-          <ButtonWithIcon name="menu" />
-          <ButtonWithIcon name="search" />
+          <ButtonWithIcon name="menu" class="header__iconMenu" />
+          <ButtonWithIcon name="search" class="header__iconSearch" />
         </div>
 
         <div class="header__right">
           <div class="header__block">
             <!-- <ButtonWithIcon name="email" /> -->
-            <ButtonWithIcon name="phone" />
-            <ButtonWithIcon name="chat" />
+            <ButtonWithIcon name="phone" class="header__iconPhone" />
+            <ButtonWithIcon name="chat" class="header__iconChat" />
           </div>
 
           <div class="header__block">
-            <ButtonWithIcon name="account" />
+            <ButtonWithIcon name="account" class="header__iconAccount" />
             <!-- <ButtonWithIcon name="favorite" /> -->
-            <ButtonWithIcon name="cart" />
+            <ButtonWithIcon name="cart" class="header__iconCart" />
           </div>
         </div>
       </div>
@@ -29,7 +29,6 @@
 .header {
   position: fixed;
   display: flex;
-  // justify-content: space-between;
   align-items: center;
   width: 100%;
   height: 80px;
@@ -63,16 +62,162 @@
     @media (max-width: 768px) {
       gap: 30px;
     }
-
-    // @media (max-width: 320px) {
-    //   gap: 0;
-    // }
   }
 
   &__block {
     display: flex;
     align-items: center;
     width: fit-content;
+  }
+
+  &__iconMenu {
+    animation: iconMenu 1s ease;
+  }
+
+  &__iconSearch {
+    animation: iconSearch 1s ease;
+  }
+
+  &__iconCart {
+    animation: iconCart 1s ease;
+  }
+
+  &__iconAccount {
+    animation: iconAccount 1s ease;
+  }
+
+  &__iconPhone {
+    animation: iconPhone 1s ease;
+  }
+
+  &__iconChat {
+    animation: iconChat 1s ease;
+  }
+}
+
+@keyframes iconChat {
+  0% {
+    opacity: 0;
+    transform: translateY(200%);
+  }
+  65% {
+    opacity: 0;
+    transform: translateY(200%);
+  }
+  80% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+  90% {
+    transform: scale(1.2);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+
+@keyframes iconPhone {
+  0% {
+    opacity: 0;
+    transform: translateY(-150%);
+  }
+  65% {
+    opacity: 0;
+    transform: translateY(-150%);
+  }
+  80% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+  90% {
+    transform: scale(1.2);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+
+@keyframes iconAccount {
+  0% {
+    opacity: 0;
+    transform: translateY(100%);
+  }
+  57% {
+    opacity: 0;
+    transform: translateY(100%);
+  }
+  75% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+  90% {
+    transform: scale(1.2);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+
+@keyframes iconCart {
+  0% {
+    opacity: 0;
+    transform: translateX(100%);
+  }
+  70% {
+    opacity: 0;
+    transform: translateX(100%);
+  }
+  75% {
+    opacity: 1;
+    transform: translateX(0);
+  }
+  90% {
+    transform: scale(1.2);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+
+@keyframes iconSearch {
+  0% {
+    opacity: 0;
+    transform: translateY(-100%);
+  }
+  70% {
+    opacity: 0;
+    transform: translateY(-100%);
+  }
+  80% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+  90% {
+    transform: scale(1.2);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+
+@keyframes iconMenu {
+  0% {
+    opacity: 0;
+    transform: translateX(-100%);
+  }
+  65% {
+    opacity: 0;
+    transform: translateX(-100%);
+  }
+  70% {
+    opacity: 1;
+    transform: translateX(0);
+  }
+  90% {
+    transform: scale(1.2);
+  }
+  100% {
+    transform: scale(1);
   }
 }
 
