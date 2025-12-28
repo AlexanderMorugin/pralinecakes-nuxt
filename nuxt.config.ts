5b9c02a00bc8774f3bb17301719749179dac38f3
Alexander Morugin <nobilis@bk.ru>
2025-12-28 15:16:36 +0300

    fix scale

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,7 +23,8 @@ export default defineNuxtConfig({
       meta: [
         {
           name: "viewport",
-          content: "width=device-width, initial-scale=1",
+          // content: "width=device-width, initial-scale=1",
+          content: "width=device-width, initial-scale=1.0, maximum-scale=1.0",
         },
         {
           name: "description",
