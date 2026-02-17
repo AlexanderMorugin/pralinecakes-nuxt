f55f36bf618fb5b832e0af72380e62c31de6f3ea
Alexander Morugin <nobilis@bk.ru>
2026-02-17 18:47:42 +0300

    one more order

--- stores/order.ts
+++ stores/order.ts
@@ -36,7 +36,9 @@ export const useOrderStore = defineStore("orderStore", () => {
   const order = ref<IOrder | null>(null);
 
   const createOrder = async (formData: IOrder) => {
-    // const result = await useFetch("/api/orders/create-order", {
+    // const result = await useFetch(
+    //   "http://localhost:3000/api/orders/create-order",
+    //   {
     const result = await useFetch(
       "http://91.229.11.141/api/orders/create-order",
       {
@@ -48,13 +50,13 @@ export const useOrderStore = defineStore("orderStore", () => {
     if (result.status.value === "success") {
       order.value = formData;
 
-      // const data = await $fetch("/api/message/send", {
-      //   method: "POST",
-      //   body: {
-      //     subject: `Заказ ${formData.order_number}`,
-      //     message: `Новый заказ ${formData.order_number}, проверить на https://praline-crm-nuxt.vercel.app/orders/`,
-      //   },
-      // });
+      const data = await $fetch("/api/message/send", {
+        method: "POST",
+        body: {
+          subject: `Заказ ${formData.order_number}`,
+          message: `Новый заказ ${formData.order_number}, проверить на https://praline-crm-nuxt.vercel.app/orders/`,
+        },
+      });
 
       // if (data) return result;
 
