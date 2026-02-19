bb8d994d5627381c18de801c7ca52e7ce68a225d
Alexander Morugin <nobilis@bk.ru>
2026-02-19 19:12:13 +0300

    fix ssr

--- stores/cakes.ts
+++ stores/cakes.ts
@@ -9,8 +9,8 @@ export const useCakesStore = defineStore("cakesStore", () => {
 
   const loadCakes = async () => {
     const result = await useFetch("/api/cakes/load-cakes", {
-      server: true,
-      lazy: false,
+      // server: true,
+      // lazy: false,
       method: "GET",
     });
 
