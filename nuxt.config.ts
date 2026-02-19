9d2c38667a796c9d39a4e81ae5fb3c311ba471a0
Alexander Morugin <nobilis@bk.ru>
2026-02-19 19:09:23 +0300

    ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  ssr: false,
+  // ssr: false,
   css: [
     "~/assets/styles/global.scss",
     "~/assets/styles/_variables.scss",
