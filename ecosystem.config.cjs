403b85c5d9effc5f8105a98c6141a3f1478725f6
Alexander Morugin <nobilis@bk.ru>
2026-02-19 20:04:08 +0300

    env

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,11 +1,16 @@
 module.exports = {
   apps: [
     {
-      name: "pralineshop",
+      name: "pralineshop.ru",
       port: "3000",
       exec_mode: "cluster",
       instances: "max",
       script: "./.output/server/index.mjs",
+      env: {
+        DATABASE_URL:
+          "postgresql://admin:Cosmos1973@193.227.240.14:5433/praline_db?schema=public",
+        BASE_URL: "https://pralineshop.ru",
+      },
     },
   ],
 };
