9b4c7535e32b5c9060a1e4c0e4ea9e76e75da3a1
Alexander Morugin <nobilis@bk.ru>
2026-02-19 19:47:58 +0300

    ssr false

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,27 +1,11 @@
-// module.exports = {
-//   apps: [
-//     {
-//       name: "pralineshop",
-//       port: "3000",
-//       exec_mode: "cluster",
-//       instances: "max",
-//       script: "./.output/server/index.mjs",
-//     },
-//   ],
-// };
-
 module.exports = {
   apps: [
     {
-      name: "nuxt-app",
-      script: "npm",
-      args: "start",
-      env: {
-        NODE_ENV: "production",
-      },
-      env_development: {
-        NODE_ENV: "development",
-      },
+      name: "pralineshop",
+      port: "3000",
+      exec_mode: "cluster",
+      instances: "max",
+      script: "./.output/server/index.mjs",
     },
   ],
 };
