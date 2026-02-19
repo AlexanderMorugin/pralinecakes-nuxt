193e40177191c4fdf91f529d77f448a7744628af
Alexander Morugin <nobilis@bk.ru>
2026-02-19 20:15:54 +0300

    nodemailer

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -11,12 +11,22 @@ module.exports = {
         DATABASE_URL:
           "postgresql://admin:Cosmos1973@193.227.240.14:5433/praline_db?schema=public",
         BASE_URL: "https://pralineshop.ru",
+        NUXT_NODEMAILER_HOST: "smtp.yandex.ru",
+        NUXT_NODEMAILER_PORT: 587,
+        NUXT_NODEMAILER_USER: "morug1n.a@ya.ru",
+        NUXT_NODEMAILER_PASSWORD: "vfkbayytfzoemsvl",
+        NUXT_NODEMAILER_CONTACT_EMAIL: "nobilis@bk.ru",
       },
       env_production: {
         NODE_ENV: "production",
         DATABASE_URL:
           "postgresql://admin:Cosmos1973@193.227.240.14:5433/praline_db?schema=public",
         BASE_URL: "https://pralineshop.ru",
+        NUXT_NODEMAILER_HOST: "smtp.yandex.ru",
+        NUXT_NODEMAILER_PORT: 587,
+        NUXT_NODEMAILER_USER: "morug1n.a@ya.ru",
+        NUXT_NODEMAILER_PASSWORD: "vfkbayytfzoemsvl",
+        NUXT_NODEMAILER_CONTACT_EMAIL: "nobilis@bk.ru",
       },
     },
   ],
--- stores/cakes.ts
+++ stores/cakes.ts
@@ -1,21 +1,18 @@
 import { defineStore } from "pinia";
 import type { IProduct } from "~/types/product";
 
-const config = useRuntimeConfig();
+// const config = useRuntimeConfig();
 
 export const useCakesStore = defineStore("cakesStore", () => {
   const cakes = ref<IProduct[] | any>([]);
   const cake = ref<IProduct | any>(null);
 
   const loadCakes = async () => {
-    const result = await useFetch(
-      "https://pralineshop.ru/api/cakes/load-cakes",
-      {
-        server: true,
-        lazy: false,
-        method: "GET",
-      },
-    );
+    const result = await useFetch("/api/cakes/load-cakes", {
+      server: true,
+      lazy: false,
+      method: "GET",
+    });
 
     if (result.status.value === "success") {
       cakes.value = result.data.value;
