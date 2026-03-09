b2cc10a72e7801127536c301688b345c1d8b9a45
Alexander Morugin <nobilis@bk.ru>
2026-03-09 18:07:01 +0300

    error page

--- error.vue
+++ error.vue
@@ -1,15 +1,37 @@
 <template>
-  <head>
-    <title>Страница не найдена — MySite</title>
-    <meta name="robots" content="noindex, nofollow" />
-  </head>
-
-  <div>
-    <h1>404 — похоже, такой страницы нет</h1>
-    <NuxtLink to="/">Главная</NuxtLink>
-  </div>
+  <WrapperPage class="errorPage">
+    <span class="errorPage__title">Ошибка 404</span>
+    <span class="errorPage__subtitle">страница не найдена</span>
+    <NuxtLink to="/" class="errorPage__link">пройдите на главную</NuxtLink>
+  </WrapperPage>
 </template>
 
-<script></script>
+<style lang="scss" scoped>
+.errorPage {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 40px;
+  height: 100vh;
 
-<style></style>
+  &__title {
+    font-family: "Montserrat-SemiBold", sans-serif;
+    font-size: 24px;
+    line-height: 32px;
+    color: var(--white-primary);
+    letter-spacing: 1px;
+  }
+
+  &__subtitle {
+    font-size: 18px;
+    line-height: 28px;
+    color: var(--white-primary);
+  }
+
+  &__link {
+    color: var(--white-primary);
+    text-decoration: underline;
+  }
+}
+</style>
